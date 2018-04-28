63dfc9a133ccfc5b3b373a8c08ec0c9dc8810d40
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 03:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animal = 'cat'
 
 function myAnimal() {
-  var animal = dog
+  var animal = 'dog'
   return animal
 }
 
@@ -13,10 +13,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
